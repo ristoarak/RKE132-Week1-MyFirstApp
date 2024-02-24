d8e8ee50964ee450de5cd2e12de99d9215982f24
ristoarak <risto.arak@tktk.ee>
2024-02-24 10:31:09 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// This is a comment tomy code
+// Rakendus küsib kasutgajal sisetada tema nime
+// Rakendustervitade kasutajatnimepidi
+
+
+
+Console.WriteLine("Enter your name");// Output
+//string -sõne
+string userNAme= Console.ReadLine();// Input
+
+
+Console.WriteLine("Hello" +", "+ userNAme +"!");
+//string iterpolation
+Console.WriteLine($"Hello, {userNAme}!");
